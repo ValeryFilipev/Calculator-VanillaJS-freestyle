6c7e1a300262f3dd9ad15f2c62cf5516cae27d87
Valery Filipyev <valeranuzhdin1998@mail.ru>
2019-02-15 12:33:28 +0300

    implement SQRT, TAN

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function shiftFn(ken) {
-  var shift = document.getElementById("shiftBtn") ;
-  var arr = document.getElementsByTagName("td") ;
+  let shift = document.getElementById("shiftBtn") ;
+  let arr = document.getElementsByTagName("td") ;
+  
   if (ken == 1) {
     shift.setAttribute("onclick", "shiftFn(0)") ;
     shift.style.backgroundColor = "orange" ;
@@ -20,11 +21,11 @@ function shiftFn(ken) {
     shift.setAttribute("onclick", "shiftFn(1)") ;
     shift.style.backgroundColor = "yellow" ;
     arr[2].innerHTML = "sin" ;
-    arr[2].setAttribute("onclick", "trigo('sin')") ;
+    arr[2].setAttribute("onclick", "trigger('sin')") ;
     arr[3].innerHTML = "cos" ;
-    arr[3].setAttribute("onclick", "trigo('cos')") ;
+    arr[3].setAttribute("onclick", "trigger('cos')") ;
     arr[4].innerHTML = "tan" ;
-    arr[4].setAttribute("onclick", "trigo('tan')") ;
+    arr[4].setAttribute("onclick", "trigger('tan')") ;
     arr[10].innerHTML = "log" ;
     arr[10].setAttribute("onclick", "log(1)") ;
     arr[28].innerHTML = "\u03C0" ;
@@ -32,11 +33,12 @@ function shiftFn(ken) {
   }
 }
 
-function input(sun) {
-  var x = document.getElementById("result") ;
-  var y = document.getElementById("myPara") ;
-  x.value += sun ;
-  y.innerHTML += sun ;
+function input(sum) {
+  let x = document.getElementById("result") ;
+  let y = document.getElementById("Parameter") ;
+  
+  x.value += sum ;
+  y.innerHTML += sum ;
 }
 
 function factorial(shirious) {
@@ -46,25 +48,28 @@ function factorial(shirious) {
   }
 }
 
-function sqrt() {
-  var x = document.getElementById("result") ;
-  var y = document.getElementById("myPara") ;
+function sqrt() {//1
+  let x = document.getElementById("result") ;
+  let y = document.getElementById("Parameter") ;
+  
   x.value += "sqrt(" ;
   y.innerHTML += (/[\d)IE]/.test(y.innerHTML.slice(-1))) ?
     " * Math.sqrt(" : "Math.sqrt(" ;
 }
 
 function leftParen() {
-  var x = document.getElementById("result") ;
-  var y = document.getElementById("myPara") ;
+  let x = document.getElementById("result") ;
+  let y = document.getElementById("Parameter") ;
+  
   x.value += "(" ;
   y.innerHTML += (/[\d)IE]/.test(y.innerHTML.slice(-1))) ?
     " * (" : "(" ;
 }
 
 function piOrE(lunar) {
-  var x = document.getElementById("result") ;
-  var y = document.getElementById("myPara") ;
+  let x = document.getElementById("result") ;
+  let y = document.getElementById("Parameter") ;
+  
   if (lunar == "pi") {
     x.value += "\u03C0" ;
     y.innerHTML += (/[\d)IE]/.test(y.innerHTML.slice(-1))) ?
@@ -77,8 +82,9 @@ function piOrE(lunar) {
 }
 
 function log(jafca) {
-  var x = document.getElementById("result") ;
-  var y = document.getElementById("myPara") ;
+  let x = document.getElementById("result") ;
+  let y = document.getElementById("Parameter") ;
+  
   if (jafca == 1) {
     x.value += "log(" ;
     y.innerHTML += (/[\d)IE]/.test(y.innerHTML.slice(-1))) ?
@@ -90,25 +96,28 @@ function log(jafca) {
   }
 }
 
-function trigo(hatsyrei) {
-  var x = document.getElementById("result") ;
-  var y = document.getElementById("myPara") ;
-  x.value += hatsyrei + "(" ;
+function trigger(neededParam) {
+  let x = document.getElementById("result") ;
+  let y = document.getElementById("Parameter") ;
+  
+  x.value += neededParam + "(" ;
   y.innerHTML += (/[\d)IE]/.test(y.innerHTML.slice(-1))) ?
-    " * Math." + hatsyrei + "(Math.PI / 180 * " : "Math." + hatsyrei + "(Math.PI / 180 * " ;
+    " * Math." + neededParam + "(Math.PI / 180 * " : "Math." + neededParam + "(Math.PI / 180 * " ;
 }
 
 function trigo1(valentin) {
-  var x = document.getElementById("result") ;
-  var y = document.getElementById("myPara") ;
+  let x = document.getElementById("result") ;
+  let y = document.getElementById("Parameter") ;
+  
   x.value += valentin + "\u207B\u00B9("
   y.innerHTML += (/[\d)IE]/.test(y.innerHTML.slice(-1))) ?
     " * 180 / Math.PI * Math.a" + valentin + "(" : "180 / Math.PI * Math.a" + valentin + "(" ;
 }
 
 function multOrDiv(edward) {
-  var x = document.getElementById("result") ;
-  var y = document.getElementById("myPara") ;
+  let x = document.getElementById("result") ;
+  let y = document.getElementById("Parameter") ;
+  
   if (edward == "mult") {
     x.value += "\u00D7" ;
     y.innerHTML += "*" ;
@@ -119,9 +128,10 @@ function multOrDiv(edward) {
 }
 
 function del() {
-  var x = document.getElementById("result") ;
-  var y = document.getElementById("myPara") ;
-  var z = document.getElementById("myAns") ;
+  let x = document.getElementById("result") ;
+  let y = document.getElementById("Parameter") ;
+  let z = document.getElementById("Answer") ;
+  
   if (x.value.slice(-3) == "Ans") {
     y.innerHTML = (/[\d)IE]/.test(x.value.slice(-4, -3))) ?
       y.innerHTML.slice(0, -(z.innerHTML.length + 3)) : y.innerHTML.slice(0, -(z.innerHTML.length)) ;
@@ -174,25 +184,28 @@ function del() {
 }
 
 function ac() {
-  var x = document.getElementById("result") ;
-  var y = document.getElementById("myPara") ;
+  let x = document.getElementById("result") ;
+  let y = document.getElementById("Parameter") ;
+  
   x.value = y.innerHTML = "" ;
 }
 
 function ans() {
-  var x = document.getElementById("result") ;
-  var y = document.getElementById("myPara") ;
-  var z = document.getElementById("myAns") ;
+  let x = document.getElementById("result") ;
+  let y = document.getElementById("Parameter") ;
+  let z = document.getElementById("Answer") ;
+  
   x.value += "Ans" ;
   y.innerHTML += (/[\d)IE]/.test(y.innerHTML.slice(-1))) ?
     " * " + z.innerHTML : z.innerHTML ;
 }
 
 function equal() {
-  var x = document.getElementById("result") ;
-  var y = document.getElementById("myPara") ;
-  var z = document.getElementById("myAns") ;
-  for (var i = 0; i < x.value.split("(").length - x.value.split(")").length; i++) {
+  let x = document.getElementById("result") ;
+  let y = document.getElementById("Parameter") ;
+  let z = document.getElementById("Answer") ;
+  
+  for (let i = 0; i < x.value.split("(").length - x.value.split(")").length; i++) {
     y.innerHTML += ")" ;
   }
   if (y.innerHTML != "") {
